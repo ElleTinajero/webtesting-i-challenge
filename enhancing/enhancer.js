4fb924c75cc110259c2bde8b099b6d90e1ffa765
Michelle <Michelle.Tinajero@outlook.com>
2020-09-10 19:42:04 -0500

    MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,21 +2,39 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
 };
 
-function succeed(item) {
-  return { ...item };
+//success test
+function succeed(name, enhancement, durability) {
+  let newItem = { name, enhancement, durability };
+  if (enhancement < 20) {
+    return { ...newItem, enhancement: enhancement + 1}
+  } else if ( enhancement === 20) {
+    return newItem;
+  }
+
+  if (enhancement > 20) {
+    throw new Error("Enhancement can't be greater than 20")
+  }
+  return newItem
+}
+//success test
+
+function fail({ name, enhancement, durability }) {
+  let newItem = { name, enhancement, durability };
+
+  if (enhancement < 15 ) {
+    return { durability: durability -5 }
+  } else if (enhancement > 16) {
+    return { durability: durability -1 }
+  } else if (enhancement >= 15) {
+    return { durability: durability -10 }
+  }
+  return newItem;
 }
 
-function fail(item) {
-  return { ...item };
-}
+function repair(item) { 
+  const newItem = { ...item, durability: 100}//returns a new item 
+  return newItem;                            //spread operator to call the item as a new object with the durability restored
+} //return the new item
 
-function repair(item) {
-  return { ...item };
-}
-
-function get(item) {
-  return { ...item };
-}
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,36 @@
-const enhancer = require('./enhancer.js');
+const { succeed, fail, repair } = require('./enhancer.js');
 // test away!
+
+describe("enhancer tests", () => {
+    test("runs without errors", () => {
+        expect(true).toBeTruthy();
+    })
+//repairs-----------------------------------------------------------------------
+    it("repairs", () => {
+        expect(repair({ name: "Weapon", enhancement: 10, })).toStrictEqual({
+            name: "Weapon", 
+            durability: 100,
+            enhancement: 10,
+        });
+    });
+//------------------------------------------------------------------------------
+
+//success-----------------------------------------------------------------------
+    it("is a success", () => {
+        expect(succeed("Weapon", 15, 50)).toEqual({
+            name: "Weapon",
+            enhancement: 16,
+            durability: 50
+        })
+    })
+//-----------------------------------------------------------------------------
+
+    it("has failed", () => {
+        expect(
+            fail({ name: "Mich", enhancement: 15, durability: 19 })
+        ).toMatchObject({
+            durability: 9,
+        })
+    })
+
+})
